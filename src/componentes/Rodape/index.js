c56e634b80fe0195b6c3d6dffff38bc98faf5ae3
luc4szin <lucas.henrique190405@gmail.com>
2023-07-19 12:15:02 -0300

    imagens

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,8 +3,8 @@ import styles from "./Rodape.module.css";
 
 export default function Rodape(props) {
   return (
-    <div>
-      <p className={styles.rodape}></p>
+    <div className={styles.rodape}>
+      <img className={styles.facebook} src={props.facebook}></img>
     </div>
   );
 }
